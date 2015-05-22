1ecaada0c8d3f3e867286961d28acd2b263572e8
Ben Carter <bcarter@redventures.com>
2015-05-21 23:34:59 -0400

    updates

--- app/config/routing.php
+++ app/config/routing.php
@@ -1,6 +1,7 @@
 <?php
 
 use GamerFind\Controllers\AuthController;
+use GamerFind\Controllers\GameListController;
 
 // Register Controllers and their dependencies
 $app['auth.controller'] = $app->share(function() use ($app) {
@@ -12,6 +13,12 @@ $app->get('/', function() use($app)  {
        $controller = new AuthController($app['twig']);
        return $controller->signup();
 });
+
+$app->get('/games', function() use($app)  {
+        $controller = new GameListController($app['twig']);
+        return $controller->getGames();
+    });
+
 
 //$app->get('/login', 'auth.controller:login');
 //$app->get('/signup', 'auth.controller:signup');
